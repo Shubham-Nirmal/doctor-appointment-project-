4b38212ffd42914e2a990dec4c9b800b16a3284a
shubham-Nirmal <shubhamnirmal2612@gmail.com>
2025-08-14 01:18:29 +0530

    creat appointment page

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -1,14 +1,16 @@
 import { createContext } from "react";
 import { doctors } from '../assets/assets'
+
 export const AppContext = createContext()
 
 const AppContextProvider = (props)=>{
 
+    const currencySymbols = '$'
 
 
 
     const value = {
-        doctors
+        doctors ,currencySymbols
     
     }
 
